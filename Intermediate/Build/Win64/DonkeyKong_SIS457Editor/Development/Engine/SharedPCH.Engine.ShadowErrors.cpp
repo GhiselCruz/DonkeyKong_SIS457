704c94af228da06d9fa960f7b9f538df0f7c62d9
Ghisel Yaritza Cruz Rocabado <ghisecr23@gmail.com>
2024-08-19 03:20:16 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/DonkeyKong_SIS457Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.40.33813
+#include "C:/Users/Ghisel Cruz/Documents/Unreal Projects/DonkeyKong_SIS457/Intermediate/Build/Win64/DonkeyKong_SIS457Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
